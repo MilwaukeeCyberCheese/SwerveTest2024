5ae444cd7547a14ccf10da71afd8760c18949aaf
Sam <techguy763@gmail.com>
2023-12-05 16:41:46 +0000

    add gyro

--- src/main/java/frc/robot/shuffleboard/SwerveTab.java
+++ src/main/java/frc/robot/shuffleboard/SwerveTab.java
@@ -4,8 +4,10 @@ import java.util.Map;
 
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.robot.Constants;
 import frc.robot.subsystems.MAXSwerveModule;
 
 public class SwerveTab {
@@ -13,36 +15,44 @@ public class SwerveTab {
         public static ShuffleboardTab swerveTab = Shuffleboard.getTab("Swerve");
 
         public static GenericEntry fLRot = swerveTab.add("FL Rotation", 0).withWidget(BuiltInWidgets.kDial)
-                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(3, 0)
+                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(2, 0)
                         .getEntry();
         public static GenericEntry fLSpeed = swerveTab.add("FL Speed", 0).withWidget(BuiltInWidgets.kNumberBar)
-                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5).withPosition(1, 0)
+                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5).withPosition(0, 0)
                         .getEntry();
 
         public static GenericEntry fRRot = swerveTab.add("FR Rotation", 0).withWidget(BuiltInWidgets.kDial)
-                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(11, 0)
+                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(12, 0)
                         .getEntry();
         public static GenericEntry fRSpeed = swerveTab.add("FR Speed", 0).withWidget(BuiltInWidgets.kNumberBar)
-                        .withPosition(9, 0)
-                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5)
+                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5).withPosition(10, 0)
                         .getEntry();
 
         public static GenericEntry bLRot = swerveTab.add("BL Rotation", 0).withWidget(BuiltInWidgets.kDial)
-                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(3, 5)
+                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(2, 5)
                         .getEntry();
         public static GenericEntry bLSpeed = swerveTab.add("BL Speed", 0).withWidget(BuiltInWidgets.kNumberBar)
-                        .withPosition(1, 5)
-                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5)
+                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5).withPosition(0, 5)
                         .getEntry();
 
         public static GenericEntry bRRot = swerveTab.add("BR Rotation", 0).withWidget(BuiltInWidgets.kDial)
-                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(11, 5)
+                        .withProperties(Map.of("min", 0, "max", 360)).withSize(5, 5).withPosition(12, 5)
                         .getEntry();
         public static GenericEntry bRSpeed = swerveTab.add("BR Speed", 0).withWidget(BuiltInWidgets.kNumberBar)
-                        .withPosition(9, 5)
-                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5)
+                        .withProperties(Map.of("min", -3, "max", 3)).withSize(2, 5).withPosition(10, 5)
                         .getEntry();
 
+        public static ComplexWidget gyroOdo = swerveTab.add(Constants.Sensors.gyro).withWidget(BuiltInWidgets.kGyro)
+                        .withProperties(Map.of("major tick spacing", 30)).withSize(3, 4).withPosition(7, 4);
+
+        /**
+         * Add positions of swerve modules to the dashboard
+         * 
+         * @param m_frontLeft  front left module
+         * @param m_frontRight front right module
+         * @param m_backLeft   back left module
+         * @param m_backRight  back right module
+         */
         public static void logSwerve(MAXSwerveModule m_frontLeft, MAXSwerveModule m_frontRight,
                         MAXSwerveModule m_backLeft,
                         MAXSwerveModule m_backRight) {
--- src/main/java/frc/robot/shuffleboard/VisionTab.java
+++ src/main/java/frc/robot/shuffleboard/VisionTab.java
@@ -1,22 +1,14 @@
 package frc.robot.shuffleboard;
 
-import java.util.Map;
-
-import edu.wpi.first.cscore.HttpCamera;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class VisionTab {
     public static ShuffleboardTab visionTab = Shuffleboard.getTab("Vision");
 
-    HttpCamera leftCam = new HttpCamera("Lefty Camera", "http://frcvision.local:1181/stream.mjpg");
-    HttpCamera rightCam = new HttpCamera("Righty Camera", "http://frcvision.local:1183/stream.mjpg")
-    //TODO put proper url
+    //TODO send cameras
+    
 
-    visionTab.add(leftCam);
-    visionTab.add(rightCam);
 
     
 
