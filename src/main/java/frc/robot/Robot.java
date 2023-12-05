6fc7531a65a5c5614993aed649da2f54b3f4a68b
Sam <techguy763@gmail.com>
2023-12-05 20:41:41 +0000

    oops we need that

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -76,6 +76,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
