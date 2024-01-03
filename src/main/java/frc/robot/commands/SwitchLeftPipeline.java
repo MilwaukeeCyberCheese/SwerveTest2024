e2627e2c6f7945550d65b92f7664ba1e3c6d152c
Milwaukee Cyber Cheese <98500141+MKECyberCheese@users.noreply.github.com>
2024-01-03 17:05:18 -0600

    add seperate commands for switching camera pipelines

---
+++ src/main/java/frc/robot/commands/SwitchLeftPipeline.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CameraSubsystem;
+import java.util.function.IntSupplier;
+
+public class SwitchLeftPipeline extends Command {
+    private CameraSubsystem m_cameraSubsystem;
+    private IntSupplier m_pipelineIndex;
+
+    /**
+     * Switch pipeline of a camera
+     * 
+     * @param cameraSubsystem subsystem containing the cameras
+     *                        {@link frc.robot.subsystems.CameraSubsystem link}
+     * @param pipelineIndex   the number of the pipeline to switch to
+     */
+    public SwitchLeftPipeline(CameraSubsystem cameraSubsystem, IntSupplier pipelineIndex) {
+        m_cameraSubsystem = cameraSubsystem;
+        m_pipelineIndex = pipelineIndex;
+        addRequirements(m_cameraSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        CameraSubsystem.switchIndex(m_pipelineIndex.getAsInt(), 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
--- src/main/java/frc/robot/commands/SwitchRightPipeline.java
+++ src/main/java/frc/robot/commands/SwitchRightPipeline.java
@@ -4,10 +4,9 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CameraSubsystem;
 import java.util.function.IntSupplier;
 
-public class SwitchPipeline extends Command {
+public class SwitchRightPipeline extends Command {
     private CameraSubsystem m_cameraSubsystem;
     private IntSupplier m_pipelineIndex;
-    private IntSupplier m_cameraNum;
 
     /**
      * Switch pipeline of a camera
@@ -15,18 +14,16 @@ public class SwitchPipeline extends Command {
      * @param cameraSubsystem subsystem containing the cameras
      *                        {@link frc.robot.subsystems.CameraSubsystem link}
      * @param pipelineIndex   the number of the pipeline to switch to
-     * @param cameraNum       the number of the camera that you want to switch
      */
-    public SwitchPipeline(CameraSubsystem cameraSubsystem, IntSupplier pipelineIndex, IntSupplier cameraNum) {
+    public SwitchRightPipeline(CameraSubsystem cameraSubsystem, IntSupplier pipelineIndex) {
         m_cameraSubsystem = cameraSubsystem;
         m_pipelineIndex = pipelineIndex;
-        m_cameraNum = cameraNum;
         addRequirements(m_cameraSubsystem);
     }
 
     @Override
     public void initialize() {
-        CameraSubsystem.switchIndex(m_pipelineIndex.getAsInt(), m_cameraNum.getAsInt());
+        CameraSubsystem.switchIndex(m_pipelineIndex.getAsInt(), 1);
     }
 
     @Override
